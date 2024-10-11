2cefeb051c6ea7a64d219d84badb29f070e75725
Matheus Lopes <inolopesm@gmail.com>
2024-10-11 13:38:56 -0300

    admin page cards

--- src/pages/admin-page.tsx
+++ src/pages/admin-page.tsx
@@ -1,4 +1,13 @@
-import { Cake as CakeIcon } from "@phosphor-icons/react/dist/ssr";
+import {
+  Cake as CakeIcon,
+  CalendarDots as CalendarDotsIcon,
+  CarProfile as CarProfileIcon,
+  ChartLine as ChartLineIcon,
+  ChatDots as ChatDotsIcon,
+  ClipboardText as ClipboardTextIcon,
+  Money as MoneyIcon,
+  Question as QuestionIcon,
+} from "@phosphor-icons/react/dist/ssr";
 import { useEffect } from "react";
 import { api } from "../lib/api";
 
@@ -11,8 +20,8 @@ export function Component() {
 
   return (
     <div>
-      <div>Dashboard</div>
-      <div className="grid grid-cols-5">
+      <div className="font-serif text-2xl">Dashboard</div>
+      <div className="mt-6 grid grid-cols-6 gap-4">
         <div className="grid gap-3 rounded-md border border-gray-300 p-3 text-center">
           <div className="text-sm font-medium text-gray-600">
             Aniversariantes do mês
@@ -21,10 +30,71 @@ export function Component() {
           <div className="text-4xl font-semibold">0</div>
           <div className="text-xs text-gray-600">Clientes cadastrados: 0</div>
         </div>
-        <div>Veículos em estoque: -1 de -1 próprios e -1 consignados</div>
-        <div>Média de dias do estoque: -1</div>
-        <div>Vendas mês atual: -1 comparado com -1 do mês anterior</div>
-        <div>Avaliações pendentes: -1 (Avaliações não concluídas)</div>
+        <div className="grid gap-3 rounded-md border border-gray-300 p-3 text-center">
+          <div className="text-sm font-medium text-gray-600">
+            Veículos no estoque
+          </div>
+          <CarProfileIcon className="mx-auto size-10 text-primary" />
+          <div className="text-4xl font-semibold">0</div>
+          <div className="text-xs text-gray-600">Próprios/Consignados: 0/0</div>
+        </div>
+        <div className="grid gap-3 rounded-md border border-gray-300 p-3 text-center">
+          <div className="text-sm font-medium text-gray-600">
+            Média de dias do estoque
+          </div>
+          <CalendarDotsIcon className="mx-auto size-10 text-primary" />
+          <div className="text-4xl font-semibold">0</div>
+          <div className="h-5 text-xs text-gray-600" />
+        </div>
+        <div className="grid gap-3 rounded-md border border-gray-300 p-3 text-center">
+          <div className="text-sm font-medium text-gray-600">
+            Vendas mês atual
+          </div>
+          <MoneyIcon className="mx-auto size-10 text-primary" />
+          <div className="text-4xl font-semibold">0</div>
+          <div className="text-xs text-gray-600">Vendas mês anterior: 0</div>
+        </div>
+        <div className="grid gap-3 rounded-md border border-gray-300 p-3 text-center">
+          <div className="text-sm font-medium text-gray-600">
+            Avaliações pendentes
+          </div>
+          <ClipboardTextIcon className="mx-auto size-10 text-primary" />
+          <div className="text-4xl font-semibold">0</div>
+          <div className="text-xs text-gray-600">Avaliações não concluídas</div>
+        </div>
+        <div className="grid gap-3 rounded-md border border-gray-300 p-3 text-sm">
+          <div className="text-center font-medium  text-gray-600">Dúvidas?</div>
+          <div className="grid gap-0.5">
+            <button
+              className="inline-flex gap-1 text-left font-medium hover:underline"
+              type="button"
+            >
+              <QuestionIcon className="mt-0.5 size-4 shrink-0" />
+              Vídeos de treinamento
+            </button>
+            <button
+              className="inline-flex gap-1 text-left font-medium hover:underline"
+              type="button"
+            >
+              <ChatDotsIcon className="mt-0.5 size-4 shrink-0" />
+              Abrir chamado
+            </button>
+            <button
+              className="inline-flex gap-1 text-left font-medium hover:underline"
+              type="button"
+            >
+              <ChartLineIcon className="mt-0.5 size-4 shrink-0" />
+              Dashboard Geral
+            </button>
+            <button
+              className="inline-flex gap-1 text-left font-medium hover:underline"
+              type="button"
+            >
+              <CalendarDotsIcon className="mt-0.5 size-4 shrink-0" />
+              Treinamento Personalizado 30 min
+            </button>
+          </div>
+        </div>
       </div>
       <div>
         Cadastros recentes (veiculo, placa, ano): V1 / P1 / A1, V2 / P2 / A2...
